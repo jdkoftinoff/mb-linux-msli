40d0c37da0bfdf50168149a91d9984b0797a93d4
Jeff Koftinoff <jeffk@meyersound.com>
2010-11-26 21:40:01 -0800

    linux drivers/net imported from msli svn

---
+++ uClinux-dist/linux-2.6.x/drivers/net/xilinx_lltemac/xlltemac_common.h
@@ -0,0 +1,259 @@
+#ifndef XLLTEMAC_COMMON_H
+#define XLLTEMAC_COMMON_H
+
+/* spinlock for hardware registers access */
+extern spinlock_t XTE_spinlock;
+
+#define XTE_AUTOSTRIPPING 1
+
+/*
+ * Our private per device data.  When a net_device is allocated we will
+ * ask for enough extra space for this.
+ */
+struct xlltemac_net_local {
+  struct list_head rcv;
+  struct list_head xmit;
+  
+  struct net_device *ndev;	/* this device */
+  struct net_device *next_dev;	/* The next device in dev_list */
+  struct net_device_stats stats;	/* Statistics for this device */
+  struct timer_list phy_timer;	/* PHY monitoring timer */
+  
+  u32 index;		/* Which interface is this */
+#if 0
+  XInterruptHandler Isr;	/* Pointer to the XLlTemac ISR routine */
+#endif
+  u8 gmii_addr;		/* The GMII address of the PHY */
+  
+  u8 led_blink;           /* slow blink state */
+  
+  u8 reset_flag;          /* reset flag -- originally 0,
+			     set to 1 to indicate that subsequent phy
+			     resets will be necessary  */
+  
+  /* The underlying OS independent code needs space as well.  A
+   * pointer to the following XLlTemac structure will be passed to
+   * any XLlTemac_ function that requires it.  However, we treat the
+   * data as an opaque object in this file (meaning that we never
+   * reference any of the fields inside of the structure). */
+  XLlFifo Fifo;
+  XLlDma Dma;
+  XLlTemac Emac;
+  
+  unsigned int fifo_irq;	/* fifo irq */
+  unsigned int dma_irq_s;	/* send irq */
+  unsigned int dma_irq_r;	/* recv irq */
+  unsigned int max_frame_size;
+  
+  int cur_speed;
+  
+  /* Buffer Descriptor space for both TX and RX BD ring */
+  void *desc_space;	/* virtual address of BD space */
+  dma_addr_t desc_space_handle;	/* physical address of BD space */
+  int desc_space_size;	/* size of BD space */
+  
+  /* buffer for one skb in case no room is available for transmission */
+  struct sk_buff *deferred_skb;
+  
+  /* send buffers for non tx-dre hw */
+  void **tx_orig_buffers;	/* Buffer addresses as returned by
+				   dma_alloc_coherent() */
+  void **tx_buffers;	/* Buffers addresses aligned for DMA */
+  dma_addr_t *tx_phys_buffers;	/* Buffer addresses in physical memory */
+  size_t tx_buffers_cur;	/* Index of current buffer used */
+  
+  /* stats */
+  int max_frags_in_a_packet;
+  unsigned long realignments;
+  unsigned long tx_hw_csums;
+  unsigned long rx_hw_csums;
+  unsigned long local_features;
+#if ! XTE_AUTOSTRIPPING
+  unsigned long stripping;
+#endif
+};
+
+/*
+ * Wrap certain temac routines with a lock, so access to the shared hard temac
+ * interface is accessed mutually exclusive for dual channel temac support.
+ */
+
+static inline void _XLlTemac_Start(XLlTemac *InstancePtr)
+{
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  XLlTemac_Start(InstancePtr);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+}
+
+static inline void _XLlTemac_Stop(XLlTemac *InstancePtr)
+{
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  XLlTemac_Stop(InstancePtr);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+}
+
+static inline void _XLlTemac_Reset(XLlTemac *InstancePtr, int HardCoreAction)
+{
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  XLlTemac_Reset(InstancePtr, HardCoreAction);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+}
+
+static inline int _XLlTemac_SetMacAddress(XLlTemac *InstancePtr,
+					  void *AddressPtr)
+{
+  int status;
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  status = XLlTemac_SetMacAddress(InstancePtr, AddressPtr);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+  
+  return status;
+}
+
+static inline void _XLlTemac_GetMacAddress(XLlTemac *InstancePtr,
+					   void *AddressPtr)
+{
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  XLlTemac_GetMacAddress(InstancePtr, AddressPtr);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+}
+
+static inline int _XLlTemac_SetOptions(XLlTemac *InstancePtr, u32 Options)
+{
+  int status;
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  status = XLlTemac_SetOptions(InstancePtr, Options);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+  
+  return status;
+}
+
+static inline int _XLlTemac_ClearOptions(XLlTemac *InstancePtr, u32 Options)
+{
+  int status;
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  status = XLlTemac_ClearOptions(InstancePtr, Options);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+  
+  return status;
+}
+
+static inline u16 _XLlTemac_GetOperatingSpeed(XLlTemac *InstancePtr)
+{
+  u16 speed;
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  speed = XLlTemac_GetOperatingSpeed(InstancePtr);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+  
+  return speed;
+}
+
+static inline void _XLlTemac_SetOperatingSpeed(XLlTemac *InstancePtr,
+					       u16 Speed)
+{
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  XLlTemac_SetOperatingSpeed(InstancePtr, Speed);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+  
+  /*
+    We need a delay after we set the speed. Otherwise the PHY
+    will not be ready.
+  */
+  udelay(10000);
+}
+
+static inline int _XLlTemac_MulticastAdd(XLlTemac *InstancePtr,
+					 void *AddressPtr, int Entry)
+{
+  int status;
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  status = XLlTemac_MulticastAdd(InstancePtr, AddressPtr, Entry);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+  
+  return status;
+}
+
+static inline int _XLlTemac_MulticastClear(XLlTemac *InstancePtr, int Entry)
+{
+  int status;
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  status = XLlTemac_MulticastClear(InstancePtr, Entry);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+  
+  return status;
+}
+
+static inline int _XLlTemac_SetMacPauseAddress(XLlTemac *InstancePtr,
+					       void *AddressPtr)
+{
+  int status;
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  status = XLlTemac_SetMacPauseAddress(InstancePtr, AddressPtr);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+  
+  return status;
+}
+
+static inline void _XLlTemac_GetMacPauseAddress(XLlTemac *InstancePtr,
+						void *AddressPtr)
+{
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  XLlTemac_GetMacPauseAddress(InstancePtr, AddressPtr);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+}
+
+static inline int _XLlTemac_GetSgmiiStatus(XLlTemac *InstancePtr, u16 *SpeedPtr)
+{
+  int status;
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  status = XLlTemac_GetSgmiiStatus(InstancePtr, SpeedPtr);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+  
+  return status;
+}
+
+static inline int _XLlTemac_GetRgmiiStatus(XLlTemac *InstancePtr,
+					   u16 *SpeedPtr,
+					   int *IsFullDuplexPtr,
+					   int *IsLinkUpPtr)
+{
+  int status;
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  status = XLlTemac_GetRgmiiStatus(InstancePtr, SpeedPtr, IsFullDuplexPtr,
+				   IsLinkUpPtr);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+  
+  return status;
+}
+
+#endif
---
+++ uClinux-dist/linux-2.6.x/drivers/net/xilinx_lltemac/xlltemac_phy_common.c
@@ -0,0 +1,637 @@
+/*
+ * xlltemac_phy_common.c
+ *
+ * (c) Copytight 2010 Meyer Sound Laboratories, Inc.
+ * (c) Copyright 2005-2008 Xilinx Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/mm.h>
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
+#include <linux/mii.h>
+#include <linux/delay.h>
+#include <linux/dma-mapping.h>
+#include <linux/xilinx_devices.h>
+#include <asm/io.h>
+#include <linux/ethtool.h>
+#include <linux/vmalloc.h>
+
+#include "xbasic_types.h"
+#include "xlltemac.h"
+#include "xllfifo.h"
+#include "xlldma.h"
+#include "xlldma_bdring.h"
+#include "xlltemac_common.h"
+#include "xlltemac_phy_common.h"
+#include "xlltemac_phy_marvell.h"
+
+void _XLlTemac_PhySetMdioDivisor(XLlTemac *InstancePtr,u8 Divisor)
+{
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  XLlTemac_PhySetMdioDivisor(InstancePtr, Divisor);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+}
+
+void _XLlTemac_PhyRead(XLlTemac *InstancePtr, u32 PhyAddress,
+		       u32 RegisterNum, u16 *PhyDataPtr)
+{
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  XLlTemac_PhyRead(InstancePtr, PhyAddress, RegisterNum, PhyDataPtr);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+}
+
+void _XLlTemac_PhyWrite(XLlTemac *InstancePtr, u32 PhyAddress,
+			u32 RegisterNum, u16 PhyData)
+{
+  unsigned long flags;
+  
+  spin_lock_irqsave(&XTE_spinlock, flags);
+  XLlTemac_PhyWrite(InstancePtr, PhyAddress, RegisterNum, PhyData);
+  spin_unlock_irqrestore(&XTE_spinlock, flags);
+}
+
+/*
+ * Perform any necessary special phy setup.
+ */
+void xlltemac_phy_setup(struct xlltemac_net_local *lp)
+{
+#ifdef CONFIG_XILINX_LLTEMAC_NATIONAL_DP83865_GMII
+  
+  u16 RegValue;
+  
+  printk(KERN_INFO "NATIONAL DP83865 PHY\n");
+  RegValue = NATIONAL_DP83865_CONTROL_INIT;
+  /*Do not reset phy*/
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr,
+		     NATIONAL_DP83865_CONTROL, RegValue);
+  
+  _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr,
+		    NATIONAL_DP83865_STATUS, &RegValue);
+  
+  _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr,
+		    NATIONAL_DP83865_STATUS, &RegValue);
+#endif
+  
+#ifdef PHY_VITESSE_8211_SGMII
+  u16 Register;
+  /* Setup the MAC into SGMII mode */
+  Register = 0xAA23;
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, 31, 0x0000);
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, 23, Register);
+  
+  /* Set SIGDET as an input */
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, 31, 0x0001);
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, 19, 0x0002);
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, 31, 0x0000);
+  
+  /* Signal all possible autonegotiation modes */
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr,  MII_ADVERTISE,
+		     ADVERTISE_ALL);
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, MII_EXADVERTISE,
+		     ADVERTISE_1000FULL| ADVERTISE_1000HALF);
+  
+  /*
+   * Reset the PHY
+   */
+  _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr, MII_BMCR, &Register);
+  Register |= BMCR_RESET;
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, MII_BMCR, Register);
+#endif
+  
+#if defined(CONFIG_XILINX_LLTEMAC_MARVELL_88E1111_RGMII) \
+  || defined(CONFIG_XILINX_LLTEMAC_MARVELL_88E1111_GMII) \
+  || defined(CONFIG_XILINX_LLTEMAC_MARVELL_88E1112_GMII)
+  xlltemac_phy_setup_marvell(lp);
+#endif
+}
+
+
+int xlltemac_renegotiate_speed_init(struct net_device *dev, int speed,
+				    DUPLEX duplex)
+{
+  struct xlltemac_net_local *lp =
+    (struct xlltemac_net_local *) netdev_priv(dev);
+  u16 phy_reg0 = BMCR_ANENABLE | BMCR_ANRESTART;
+  u16 phy_reg4;
+  u16 phy_reg9 = 0;
+  
+  
+  /*
+   * It appears that the 10baset full and half duplex settings
+   * are overloaded for gigabit ethernet
+   */
+  if (speed == 0) {
+    phy_reg4 = ADVERTISE_10FULL | ADVERTISE_10HALF 
+      | ADVERTISE_100FULL | ADVERTISE_100HALF | ADVERTISE_CSMA;
+    phy_reg9 = EX_ADVERTISE_1000FULL | EX_ADVERTISE_1000HALF;
+    
+  }
+  else if ((duplex == FULL_DUPLEX) && (speed == 10)) {
+    phy_reg4 = ADVERTISE_10FULL | ADVERTISE_CSMA;
+  }
+  else if ((duplex == FULL_DUPLEX) && (speed == 100)) {
+    phy_reg4 = ADVERTISE_100FULL | ADVERTISE_CSMA;
+  }
+  else if ((duplex == FULL_DUPLEX) && (speed == 1000)) {
+    phy_reg4 = ADVERTISE_CSMA;
+    phy_reg9 = EX_ADVERTISE_1000FULL;
+  }
+  else if (speed == 10) {
+    phy_reg4 = ADVERTISE_10HALF | ADVERTISE_CSMA;
+  }
+  else if (speed == 100) {
+    phy_reg4 = ADVERTISE_100HALF | ADVERTISE_CSMA;
+  }
+  else if (speed == 1000) {
+    phy_reg4 = ADVERTISE_CSMA;
+    phy_reg9 = EX_ADVERTISE_1000HALF;
+  }
+  else {
+    printk(KERN_ERR
+	   "%s: XLlTemac: unsupported speed requested: %d\n",
+	   dev->name, speed);
+    return -1;
+  }
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, MII_ADVERTISE, phy_reg4);
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, MII_EXADVERTISE, phy_reg9);
+  /* initiate an autonegotiation of the speed */
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, MII_BMCR, phy_reg0);
+  printk(KERN_INFO
+	 "%s: XLlTemac: Autonegotiation started\n", dev->name);
+  return 0;
+}
+
+int xlltemac_renegotiate_speed_wait(struct net_device *dev, int speed,
+				    DUPLEX duplex)
+{
+  struct xlltemac_net_local *lp
+    = (struct xlltemac_net_local *) netdev_priv(dev);
+  int retries = 2;
+  int wait_count;
+  u16 phy_reg0 = BMCR_ANENABLE | BMCR_ANRESTART;
+  u16 phy_reg1;
+  u16 phy_reg4;
+  u16 phy_reg9 = 0;
+  
+  
+  /*
+   * It appears that the 10baset full and half duplex settings
+   * are overloaded for gigabit ethernet
+   */
+  if (speed == 0) {
+    phy_reg4 = ADVERTISE_10FULL | ADVERTISE_10HALF 
+      | ADVERTISE_100FULL | ADVERTISE_100HALF | ADVERTISE_CSMA;
+    phy_reg9 = EX_ADVERTISE_1000FULL | EX_ADVERTISE_1000HALF;
+    
+  }
+  else if ((duplex == FULL_DUPLEX) && (speed == 10)) {
+    phy_reg4 = ADVERTISE_10FULL | ADVERTISE_CSMA;
+  }
+  else if ((duplex == FULL_DUPLEX) && (speed == 100)) {
+    phy_reg4 = ADVERTISE_100FULL | ADVERTISE_CSMA;
+  }
+  else if ((duplex == FULL_DUPLEX) && (speed == 1000)) {
+    phy_reg4 = ADVERTISE_CSMA;
+    phy_reg9 = EX_ADVERTISE_1000FULL;
+  }
+  else if (speed == 10) {
+    phy_reg4 = ADVERTISE_10HALF | ADVERTISE_CSMA;
+  }
+  else if (speed == 100) {
+    phy_reg4 = ADVERTISE_100HALF | ADVERTISE_CSMA;
+  }
+  else if (speed == 1000) {
+    phy_reg4 = ADVERTISE_CSMA;
+    phy_reg9 = EX_ADVERTISE_1000HALF;
+  }
+  else {
+    printk(KERN_ERR
+	   "%s: XLlTemac: unsupported speed requested: %d\n",
+	   dev->name, speed);
+    return -1;
+  }
+  
+  /*
+   * link status in register 1:
+   * first read / second read:
+   * 0               0           link is down
+   * 0               1           link is up (but it was down earlier)
+   * 1               0           link is down (but it was just up)
+   * 1               1           link is up
+   *
+   */
+  _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr, MII_BMSR, &phy_reg1);
+  _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr, MII_BMSR, &phy_reg1);
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, MII_ADVERTISE, phy_reg4);
+  _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, MII_EXADVERTISE, phy_reg9);
+  
+  while (retries--) {
+    /* initiate an autonegotiation of the speed */
+    _XLlTemac_PhyWrite(&lp->Emac, lp->gmii_addr, MII_BMCR, phy_reg0);
+    
+    wait_count = 20;	/* so we don't loop forever */
+    while (wait_count--) {
+      /* wait a bit for the negotiation to complete */
+      mdelay(500);
+      _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr, MII_BMSR,
+			&phy_reg1);
+      _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr, MII_BMSR,
+			&phy_reg1);
+      
+      if ((phy_reg1 & BMSR_LSTATUS) &&
+	  (phy_reg1 & BMSR_ANEGCOMPLETE))
+	break;
+      
+    }
+    
+    if (phy_reg1 & BMSR_LSTATUS) {
+      printk(KERN_INFO
+	     "%s: XLlTemac: We renegotiated the speed to: %d\n",
+	     dev->name, speed);
+      return 0;
+    }
+    else {
+      printk(KERN_ERR
+	     "%s: XLlTemac: Not able to set the speed to %d (status: 0x%0x)\n",
+	     dev->name, speed, phy_reg1);
+      return -1;
+    }
+  }
+  
+  printk(KERN_ERR
+	 "%s: XLlTemac: Not able to set the speed to %d\n", dev->name,
+	 speed);
+  return -1;
+}
+
+/*
+ * The PHY registers read here should be standard registers in all PHY chips
+ */
+int xlltemac_get_phy_status(struct net_device *dev, DUPLEX * duplex,
+			    int *linkup)
+{
+  struct xlltemac_net_local *lp
+    = (struct xlltemac_net_local *) netdev_priv(dev);
+  u16 reg;
+  
+  _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr, MII_BMCR, &reg);
+  *duplex = FULL_DUPLEX;
+  
+#ifdef CONFIG_XILINX_LLTEMAC_NATIONAL_DP83865_GMII
+  _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr,
+		    NATIONAL_DP83865_STATUS, &reg);
+  *linkup=(reg & NATIONAL_DP83865_STATUS_LINK) != 0;
+  
+#else
+#if defined(CONFIG_XILINX_LLTEMAC_MARVELL_88E1111_GMII)		\
+  || defined(CONFIG_XILINX_LLTEMAC_MARVELL_88E1112_GMII)
+  _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr, MII_BMSR, &reg);
+  if(reg & BMSR_LSTATUS)
+    {
+      _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr,
+			MARVELL_88E1111_PHY_SPECIFIC_STATUS_REG_OFFSET,
+			&reg);
+      if((reg & MARVELL_88E1111_LINK_DUPLEX) == 0)
+	{
+	  *duplex = HALF_DUPLEX;
+	}
+      *linkup = (reg & MARVELL_88E1111_LINK_UP) != 0;
+    }
+  else
+    {
+      *linkup = 0;
+    }
+#else
+  _XLlTemac_PhyRead(&lp->Emac, lp->gmii_addr, MII_BMSR, &reg);
+  *linkup = (reg & BMSR_LSTATUS) != 0;
+#endif
+#endif
+  return 0;
+}
+
+/* Detect the PHY address by scanning addresses 0 to 31 and
+ * looking at the MII status register (register 1) and assuming
+ * the PHY supports 10Mbps full/half duplex. Feel free to change
+ * this code to match your PHY, or hardcode the address if needed.
+ */
+/* Use MII register 1 (MII status register) to detect PHY */
+#define PHY_DETECT_REG  1
+
+/* Mask used to verify certain PHY features (or register contents)
+ * in the register above:
+ *  0x1000: 10Mbps full duplex support
+ *  0x0800: 10Mbps half duplex support
+ *  0x0008: Auto-negotiation support
+ */
+#define PHY_DETECT_MASK 0x1808
+
+int xlltemac_detect_phy(struct xlltemac_net_local *lp, char *dev_name)
+{
+  u16 phy_reg;
+  u32 phy_addr;
+  
+  for (phy_addr = 1; phy_addr <= 31; phy_addr++) {
+    _XLlTemac_PhyRead(&lp->Emac, phy_addr, PHY_DETECT_REG, &phy_reg);
+    
+    if ((phy_reg != 0xFFFF) &&
+	((phy_reg & PHY_DETECT_MASK) == PHY_DETECT_MASK)) {
+      /* Found a valid PHY address */
+      printk(KERN_INFO "XTemac: PHY detected at address %d.\n", phy_addr);
+      return phy_addr;
+    }
+  }
+  
+  printk(KERN_WARNING "XTemac: No PHY detected.\n");
+  return 0;		/* default to zero */
+}
+
+
+xtenet_phy_bus_t *xtenet_phy_bus=NULL;
+
+spinlock_t xtenet_phy_bus_spinlock = SPIN_LOCK_UNLOCKED;
+
+
+void xlltemac_remove_phy(struct xlltemac_net_local *lp)
+{
+  unsigned long flags;
+  xtenet_phy_bus_t *phy_bus,**curr_phy_bus_ptr;
+  int i;
+  unsigned char mask;
+  if(lp==NULL) return;
+
+  spin_lock_irqsave(&xtenet_phy_bus_spinlock,flags);
+
+  /* find a bus */
+  for(curr_phy_bus_ptr=&xtenet_phy_bus;
+      curr_phy_bus_ptr&&*curr_phy_bus_ptr
+	&&(*curr_phy_bus_ptr)->BaseAddress!=lp->Emac.Config.PhyBaseAddress;
+      curr_phy_bus_ptr=&(*curr_phy_bus_ptr)->next);
+
+  if(curr_phy_bus_ptr&&(*curr_phy_bus_ptr))
+    {
+      phy_bus=*curr_phy_bus_ptr;
+
+      /* look for the PHY */
+      for(i=0,mask=1;i<phy_bus->n_phys;i++,mask<<=1)
+	{
+	  if(phy_bus->phys[i]==lp->gmii_addr)
+	    {
+	      /* free this PHY */
+	      phy_bus->use_mask&=~mask;
+	      printk(KERN_INFO
+		     "XLlTemac: PHY 0x%02x removed from the bus for 0x%08x\n",
+			 phy_bus->phys[i],phy_bus->BaseAddress);
+	      /* if there are no other PHYs used on this bus,
+		 remove the bus */
+	      if(phy_bus->use_mask=='\0')
+		{
+		  *curr_phy_bus_ptr=phy_bus->next;
+		  printk(KERN_INFO "XLlTemac: Deleting PHY bus for 0x%08x\n",
+			 phy_bus->BaseAddress);
+		  kfree(phy_bus);
+		}
+	      spin_unlock_irqrestore(&xtenet_phy_bus_spinlock,flags);
+	      return;
+	    }
+	}
+      printk(KERN_ERR
+	     "XLlTemac: Can't find PHY 0x%02x on the bus for 0x%08x\n",
+	     lp->gmii_addr,lp->Emac.Config.PhyBaseAddress);
+    }
+  else
+    {
+      printk(KERN_ERR "XLlTemac: Can't find a PHY bus for 0x%08x\n",
+	     lp->Emac.Config.PhyBaseAddress);
+    }
+  spin_unlock_irqrestore(&xtenet_phy_bus_spinlock,flags);
+}
+
+int xlltemac_add_phy(struct xlltemac_net_local *lp)
+{
+  unsigned long flags;
+  u16 phy_reg;
+  int phy_addr,i,finished;
+  unsigned char mask;
+  xtenet_phy_bus_t *phy_bus,*curr_phy_bus;
+
+  spin_lock_irqsave(&xtenet_phy_bus_spinlock,flags);
+
+  /* do we already know a bus for this address? */
+  for(phy_bus=xtenet_phy_bus;
+      phy_bus && phy_bus->BaseAddress!=lp->Emac.Config.BaseAddress;
+      phy_bus=phy_bus->next);
+
+  if(phy_bus)
+    {
+      /* if we do, only look for the PHY here */
+      lp->Emac.Config.PhyBaseAddress=phy_bus->BaseAddress;
+
+      finished=0;
+      phy_addr=0;
+      /* look for the first available PHY */
+      for(i=0,mask=1;i<phy_bus->n_phys;i++,mask<<=1)
+	{
+	  if((phy_bus->use_mask&mask)==0)
+	    {
+	      phy_bus->use_mask|=mask;
+	      lp->Emac.Config.PhyBaseAddress=phy_bus->BaseAddress;
+	      phy_addr=phy_bus->phys[i];
+	      i=phy_bus->n_phys;
+	      finished=1;
+	    }
+	}
+      if(finished)
+	{
+	  printk(KERN_INFO
+		 "XLlTemac: PHY bus for 0x%08x found at 0x%08x, phy 0x%02x\n",
+		 lp->Emac.Config.BaseAddress,
+		 lp->Emac.Config.PhyBaseAddress,phy_addr);
+	}     
+      else
+	{
+	  /* error, return local address and broadcast */
+	  printk(KERN_ERR
+ "XLlTemac: bus for 0x%08x is found, however there are no unused PHYs on it\n",
+		 lp->Emac.Config.BaseAddress);
+	  lp->Emac.Config.PhyBaseAddress=lp->Emac.Config.BaseAddress;
+	  spin_unlock_irqrestore(&xtenet_phy_bus_spinlock,flags);
+	  return 0;
+	}
+    }
+  else
+    {
+
+      if(lp->Emac.Config.BaseAddress!=0)
+	{
+	  /* create and scan the bus */
+	  phy_bus=kmalloc(sizeof(xtenet_phy_bus_t), GFP_KERNEL);
+
+	  if(phy_bus==NULL)
+	    {
+	      /* error, return local address and broadcast */
+	      printk(KERN_ERR
+	 "XLlTemac: can't allocate memory for PHY bus descriptor for 0x%08x\n",
+		     lp->Emac.Config.BaseAddress);
+	      lp->Emac.Config.PhyBaseAddress=lp->Emac.Config.BaseAddress;
+	      spin_unlock_irqrestore(&xtenet_phy_bus_spinlock,flags);
+	      return 0;
+	    }
+	  phy_bus->next=NULL;
+	  phy_bus->BaseAddress=lp->Emac.Config.BaseAddress;
+	  phy_bus->n_phys=0;
+	  phy_bus->use_mask=0;
+	  for(phy_addr=1;phy_addr<=31;phy_addr++)
+	    {
+	      _XLlTemac_PhyRead(&lp->Emac, phy_addr, PHY_DETECT_REG, &phy_reg);
+	      if ((phy_reg != 0xFFFF) &&
+		  ((phy_reg & PHY_DETECT_MASK) == PHY_DETECT_MASK))
+		{
+		  phy_bus->phys[phy_bus->n_phys]=phy_addr;
+#ifdef CONFIG_XILINX_LLTEMAC_MARVELL_88E1112_GMII
+		  xlltemac_leds_initialize(lp,phy_addr, LED_STATE_OFF);
+#endif
+		  phy_bus->n_phys++;
+		}
+	      
+	    }
+	}
+      else
+	{
+	  phy_bus = NULL;
+	  phy_addr = 0;
+	}
+
+      if((phy_bus!=NULL) && (phy_bus->n_phys>0))
+	{
+	  /* if any PHYs are found, register this bus */
+	  lp->Emac.Config.PhyBaseAddress=phy_bus->BaseAddress;
+	  phy_addr=phy_bus->phys[0];
+	  phy_bus->use_mask=1;
+
+	  if(xtenet_phy_bus)
+	    {
+	      for(curr_phy_bus=xtenet_phy_bus;
+		  curr_phy_bus->next;
+		  curr_phy_bus=curr_phy_bus->next);
+	      curr_phy_bus->next=phy_bus;
+	    }
+	  else
+	    {
+	      xtenet_phy_bus=phy_bus;
+	    }
+	  printk(KERN_INFO
+	     "XLlTemac: PHY bus found at 0x%08x, phys: ",
+	     lp->Emac.Config.BaseAddress);
+	  for(i=0;i<phy_bus->n_phys;i++)
+	    {
+	      printk("0x%02x",phy_bus->phys[i]);
+	      if(i==0)
+		{
+		  printk("(local)");
+		}
+	      if(i<phy_bus->n_phys-1)
+		{
+		  printk(", ");
+		}
+	    }
+	  printk("\n");
+	}
+      else
+	{
+	  /* if no PHYs found, don't register this bus, look for PHYs
+	     elsewhere */
+	  if(phy_bus != NULL)
+	    kfree(phy_bus);
+
+	  /* find a bus on the same adapter with the address 0x40 less than
+	     the current base address */
+
+	  for(phy_bus=xtenet_phy_bus;
+	      phy_bus && phy_bus->BaseAddress+0x40!=
+		lp->Emac.Config.BaseAddress;
+	      phy_bus=phy_bus->next);
+
+
+	  /* if no such bus, use the first bus */
+	  if(phy_bus==NULL)
+	    {
+	      phy_bus=xtenet_phy_bus;
+	    }
+
+	  finished=0;
+	  while(!finished)
+	    {
+	      if(phy_bus)
+		{
+		  /* look for the first available PHY */
+		  for(i=0,mask=1;i<phy_bus->n_phys;i++,mask<<=1)
+		    {
+		      if((phy_bus->use_mask&mask)==0)
+			{
+			  phy_bus->use_mask|=mask;
+			  lp->Emac.Config.PhyBaseAddress=phy_bus->BaseAddress;
+			  phy_addr=phy_bus->phys[i];
+			  i=phy_bus->n_phys;
+			  finished=1;
+			}
+		    }
+		  if(finished)
+		    {
+		      printk(KERN_INFO
+	          "XLlTemac: PHY bus for 0x%08x found at 0x%08x, phy 0x%02x\n",
+			     lp->Emac.Config.BaseAddress,
+			     lp->Emac.Config.PhyBaseAddress,phy_addr);
+		      
+		    }
+		  else
+		    {
+		      /* if found nothing, look at the first bus, or 
+			 if found nothing on the first bus, return an error */
+		      if(phy_bus==xtenet_phy_bus)
+			{
+			  phy_bus=NULL;
+			}
+		      else
+			{
+			  phy_bus=xtenet_phy_bus;
+			}
+		    }
+		}
+	      if(phy_bus==NULL)
+		{
+		  /* error, return local address and broadcast */
+		  printk(KERN_ERR
+			 "XLlTemac: no PHYs found for 0x%08x\n",
+			 lp->Emac.Config.BaseAddress);
+		  lp->Emac.Config.PhyBaseAddress=lp->Emac.Config.BaseAddress;
+		  spin_unlock_irqrestore(&xtenet_phy_bus_spinlock,flags);
+		  return 0;
+		}
+	    }
+	}
+    }
+
+  spin_unlock_irqrestore(&xtenet_phy_bus_spinlock,flags);
+  return phy_addr;
+}
