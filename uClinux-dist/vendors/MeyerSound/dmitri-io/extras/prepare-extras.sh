a99913ac63fb6f7bdbc706b534aebf0ab9d6c079
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2011-01-10 11:17:14 -0800

    avbd removed from mb-linux-msli vendors/extras, needs to be built after

--- uClinux-dist/vendors/MeyerSound/dmitri-io/extras/prepare-extras.sh
+++ uClinux-dist/vendors/MeyerSound/dmitri-io/extras/prepare-extras.sh
@@ -8,8 +8,6 @@ ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/ieee1722_1.mk lib/ieee1722_1/M
 
 ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/io-osc.mk user/io-osc/Makefile
 
-ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/avbd.mk user/avbd/Makefile
-
 ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/avbweb.mk user/avbweb/Makefile
 
 
