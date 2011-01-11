b27afd36f880bb1b19a723dfe18d9c2a79ac3f47
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2011-01-10 22:32:09 -0800

    added microsupport as dep to ieee1722_1

--- uClinux-dist/vendors/MeyerSound/dmitri-io/extras/ieee1722_1.mk
+++ uClinux-dist/vendors/MeyerSound/dmitri-io/extras/ieee1722_1.mk
@@ -1,6 +1,7 @@
 PWD:=$(shell pwd)
 include $(PWD)/../../vendors/MeyerSound/dmitri-io/extras/common.mk
 
+US_CONFIG=$(UCLIBC_LIB_DIR)/microsupport/microsupport-config
 include $(PROJECT_TOP_DIR)/project.mk
 	
 romfs : 
