5db3f38a9c5a5686ff0b9aed379d4cc274526454
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2011-02-01 10:42:24 -0800

    update-submodules

--- uClinux-dist/vendors/MeyerSound/dmitri-io/extras/prepare-extras.sh
+++ uClinux-dist/vendors/MeyerSound/dmitri-io/extras/prepare-extras.sh
@@ -4,6 +4,8 @@ ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/libtc.mk user/libtc/Makefile
 
 ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/microsupport.mk lib/microsupport/Makefile
 
+ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/avtp.mk lib/avtp/Makefile
+
 ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/ieee1722_1.mk lib/ieee1722_1/Makefile
 
 ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/io-osc.mk user/io-osc/Makefile
