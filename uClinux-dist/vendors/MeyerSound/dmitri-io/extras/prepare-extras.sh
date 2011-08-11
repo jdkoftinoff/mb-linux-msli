971f9a1e61cc9ea3e589fb77964430fa96aa2643
Jeff Koftinoff <jeff.koftinoff@ieee.org>
2011-08-11 16:41:51 -0700

    Added libtc submodule into lib folder. Not referencing libtc from user folder.  Updated Kconfig/Makefile for both lib and user to match. Updated prepare-extras to match.

--- uClinux-dist/vendors/MeyerSound/dmitri-io/extras/prepare-extras.sh
+++ uClinux-dist/vendors/MeyerSound/dmitri-io/extras/prepare-extras.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/libtc.mk user/libtc/Makefile
-
 ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/microsupport.mk lib/microsupport/Makefile
+
+ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/libtc.mk lib/libtc/Makefile
 
 ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/avdecc.mk lib/avdecc/Makefile
 
