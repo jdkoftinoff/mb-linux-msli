7852e1687d6cc32493531bdef873b6c7c4f169ec
Andre Labelle <andrel@meyersound.com>
2012-01-18 09:50:31 -0800

    compile in microsupport

---
+++ uClinux-dist/vendors/MeyerSound/CAL_ICS/extras/prepare-extras.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/microsupport.mk lib/microsupport/Makefile
+
+ln -sf "$PWD"/vendors/MeyerSound/dmitri-io/extras/cal-dsp.mk user/cal-dsp/Makefile
+
