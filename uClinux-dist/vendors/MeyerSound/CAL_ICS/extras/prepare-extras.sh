4785fcd56b3cdcd074e2f9446cc5eab807eef1e9
Andre Labelle <andrel@meyersound.com>
2012-02-27 21:50:24 -0800

    removed extras

--- uClinux-dist/vendors/MeyerSound/CAL_ICS/extras/prepare-extras.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-ln -sf "$PWD"/vendors/MeyerSound/CAL_ICS/extras/microsupport.mk lib/microsupport/Makefile
-
-ln -sf "$PWD"/vendors/MeyerSound/CAL_ICS/extras/cal-dsp.mk user/cal-dsp/Makefile
-
