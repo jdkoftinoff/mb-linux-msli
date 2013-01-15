de1b8fec809befe7d78a81256b947ce8fdb74a83
Yuriy Dragunov <yuriy.dragunov@labxtechnologies.com>
2013-01-15 10:14:24 -0500

    Renamed Biamp Labrinth-SuperIO to LabX Labrinth.

--- uClinux-dist/linux-2.6.x/arch/microblaze/platform/LabX/Labrinth-AVB/labrinth_avb_packetizer.h
+++ uClinux-dist/linux-2.6.x/arch/microblaze/platform/LabX/Labrinth-AVB/labrinth_avb_packetizer.h
@@ -1,12 +1,12 @@
 /*
- *  linux/arch/microblaze/platform/Biamp/labrinth_avb_packetizer.h
+ *  linux/arch/microblaze/platform/LabX/Labrinth-AVB/labrinth_avb_packetizer.h
  *
  *  Lab X Technologies AVB flexible audio packetizer derived driver,
  *  adding some Labrinth-specific extensions
  *
  *  Written by Eldridge M. Mount IV (eldridge.mount@labxtechnologies.com)
  *
- *  Copyright (C) 2010 Biamp Systems, All Rights Reserved.
+ *  Copyright (C) 2010 Lab X Technologies, LLC, All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
