958defd351b635f9340ec8913289172e8e204703
emount <eldridge.mount@labxtechnologies.com>
2013-07-05 17:31:16 -0400

    Added Lawo audio block driver

--- uClinux-dist/linux-2.6.x/drivers/net/labx_avb/labx_tdm_audio.c
+++ uClinux-dist/linux-2.6.x/drivers/net/labx_avb/labx_tdm_audio.c
@@ -1401,6 +1401,7 @@ static struct of_device_id tdm_of_match[] = {
   { .compatible = "xlnx,labx-tdm-audio-1.01.a", },
   { .compatible = "xlnx,labx-tdm-audio-1.02.a", },
   { .compatible = "xlnx,labx-tdm-audio-1.03.a", },
+  { .compatible = "xlnx,lawo-audio-block-1.00.b", },
   { /* end of list */ },
 };
 
