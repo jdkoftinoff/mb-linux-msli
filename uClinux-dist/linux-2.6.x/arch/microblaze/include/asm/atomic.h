c525e43e904a4538abdd2628ccd59635abff3668
Chris Wulff <chris.wulff@labxtechnologies.com>
2013-07-12 16:10:17 -0400

    microblaze ftrace: Backport from xilinx 3.9 repository.

--- uClinux-dist/linux-2.6.x/arch/microblaze/include/asm/atomic.h
+++ uClinux-dist/linux-2.6.x/arch/microblaze/include/asm/atomic.h
@@ -38,11 +38,11 @@ static inline int atomic_cmpxchg(atomic_t *v, int old, int new)
 	int ret;
 	unsigned long flags;
 
-	local_irq_save(flags);
+	raw_local_irq_save(flags);
 	ret = v->counter;
 	if (likely(ret == old))
 		v->counter = new;
-	local_irq_restore(flags);
+	raw_local_irq_restore(flags);
 
 	return ret;
 }
@@ -61,9 +61,9 @@ static inline void atomic_clear_mask(unsigned long mask, unsigned long *addr)
 {
 	unsigned long flags;
 
-	local_irq_save(flags);
+	raw_local_irq_save(flags);
 	*addr &= ~mask;
-	local_irq_restore(flags);
+	raw_local_irq_restore(flags);
 }
 
 /**
@@ -78,10 +78,10 @@ static inline int atomic_add_return(int i, atomic_t *v)
 	unsigned long flags;
 	int val;
 
-	local_irq_save(flags);
+	raw_local_irq_save(flags);
 	val = v->counter;
 	v->counter = val += i;
-	local_irq_restore(flags);
+	raw_local_irq_restore(flags);
 
 	return val;
 }
@@ -100,11 +100,11 @@ static inline int atomic_dec_if_positive(atomic_t *v)
 	unsigned long flags;
 	int res;
 
-	local_irq_save(flags);
+	raw_local_irq_save(flags);
 	res = v->counter - 1;
 	if (res >= 0)
 		v->counter = res;
-	local_irq_restore(flags);
+	raw_local_irq_restore(flags);
 
 	return res;
 }
