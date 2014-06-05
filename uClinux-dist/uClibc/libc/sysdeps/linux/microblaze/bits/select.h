4eadf7c26d6a2ca5ba9ba0cc998c3a108cf50f8b
Andre Labelle <andrel@meyersound.com>
2014-06-05 12:41:50 -0700

    fixed select macros, didn't work if the file descriptor number was greater than 32

--- uClinux-dist/uClibc/libc/sysdeps/linux/microblaze/bits/select.h
+++ uClinux-dist/uClibc/libc/sysdeps/linux/microblaze/bits/select.h
@@ -15,44 +15,16 @@
 # error "Never use <bits/select.h> directly; include <sys/select.h> instead."
 #endif
 
-#ifdef __GNUC__
-
 /* We don't use `memset' because this would require a prototype and
    the array isn't too big.  */
-#define __FD_ZERO(s)							      \
-  do {									      \
-    unsigned int __i;							      \
-    fd_set *__arr = (s);						      \
-    for (__i = 0; __i < sizeof (fd_set) / sizeof (__fd_mask); ++__i)	      \
-      __FDS_BITS (__arr)[__i] = 0;					      \
+# define __FD_ZERO(set)  \
+  do {                                                                        \
+    unsigned int __i;                                                         \
+    fd_set *__arr = (set);                                                    \
+    for (__i = 0; __i < sizeof (fd_set) / sizeof (__fd_mask); ++__i)          \
+      __FDS_BITS (__arr)[__i] = 0;                                            \
   } while (0)
+# define __FD_SET(d, set)    (__FDS_BITS (set)[__FDELT (d)] |= __FDMASK (d))
+# define __FD_CLR(d, set)    (__FDS_BITS (set)[__FDELT (d)] &= ~__FDMASK (d))
+# define __FD_ISSET(d, set)  (__FDS_BITS (set)[__FDELT (d)] & __FDMASK (d))
 
-#define __FD_SET(fd, s)						       	      \
-  do {									      \
-	int __fd = (fd);						      \
-	unsigned int *__addr = (unsigned int *)&__FDS_BITS (s);		      \
-	*__addr |= (1 << __fd);    					      \
-  } while (0)
-
-#define __FD_CLR(fd, s)							      \
-  do {									      \
-	int __fd = (fd);						      \
-	unsigned int *__addr = (unsigned int *)&__FDS_BITS (s);		      \
-	*__addr &= ~(1 << __fd);					      \
-  } while (0)
-
-#define __FD_ISSET(fd, s)						      \
-  ({									      \
-	int __fd = (fd);						      \
-	unsigned int *__addr = (unsigned int *)&__FDS_BITS (s);		      \
-	int res;							      \
-	res = (*__addr & (1 << fd)) ? 1 : 0;				      \
-  })
-
-#else /* !__GNUC__ */
-
-#define __FD_SET(d, s)     (__FDS_BITS (s)[__FDELT(d)] |= __FDMASK(d))
-#define __FD_CLR(d, s)     (__FDS_BITS (s)[__FDELT(d)] &= ~__FDMASK(d))
-#define __FD_ISSET(d, s)   ((__FDS_BITS (s)[__FDELT(d)] & __FDMASK(d)) != 0)
-
-#endif /* __GNUC__ */
