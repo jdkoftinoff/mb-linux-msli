5b4814ccad99188c078afac699473c6314eeec49
Andre Labelle <andrel@meyersound.com>
2014-09-04 11:40:29 -0700

    clean up shell script environment variables

--- local/makeimage.sh
+++ local/makeimage.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
 if [ -z "${DMITRI_IO_MAINT_DOWNLOAD_BIT}" ]; then
-  DMITRI_IO_AVB_DOWNLOAD_BIT="${HOME}/labx-ip/IO_Link/FPGA/Synthesis/IO_Link_top.bit"
+  DMITRI_IO_MAINT_DOWNLOAD_BIT="${HOME}/msli-avb/IO_Link/FPGA/Synthesis/IO_Link_top.bit"
 fi
 
 if [ -z "${DMITRI_IO_AVB_DOWNLOAD_BIT}" ]; then
-  DMITRI_IO_AVB_DOWNLOAD_BIT="${HOME}/labx-ip/IO_Link/FPGA/Synthesis/IO_Link_top.bit"
+  DMITRI_IO_AVB_DOWNLOAD_BIT="${HOME}/msli-avb/IO_Link/FPGA/Synthesis/IO_Link_top.bit"
 fi
 
-if [ -z "${DMITRI_DGPIO_AVB_DOWNLOAD_BIT}" ]; then
-  DMITRI_DGPIO_AVB_DOWNLOAD_BIT="${HOME}/labx-ip/IO_Link/FPGA_DGPIO/Synthesis/IO_Link_top.bit"
-fi
+#if [ -z "${DMITRI_DGPIO_AVB_DOWNLOAD_BIT}" ]; then
+#  DMITRI_DGPIO_AVB_DOWNLOAD_BIT="${HOME}/msli-avb/IO_Link/FPGA_DGPIO/Synthesis/IO_Link_top.bit"
+#fi
 
 if [ -z "${DMITRI_IO_MAINT_DTS}" ]; then
   DMITRI_IO_MAINT_DTS="xilinx.dts"
@@ -20,24 +20,16 @@ if [ -z "${DMITRI_IO_AVB_DTS}" ]; then
   DMITRI_IO_AVB_DTS="xilinx-avb.dts"
 fi
 
-if [ -z "${DMITRI_IO_AVB_MAINT_DTS}" ]; then
-  DMITRI_IO_AVB_MAINT_DTS="xilinx.dts"
-fi
-
-if [ -z "${DMITRI_DGPIO_AVB_DTS}" ]; then
-  DMITRI_DGPIO_AVB_DTS="xilinx-avb-dgpio.dts"
-fi
-
-if [ -z "${MBBL_ELF}" ]; then
-  MBBL_ELF="${HOME}/dmitri_io_1_GNET/mblaze/SDK/SDK_Workspace/mbbl/Release/mbbl.elf"
-fi
+#if [ -z "${MBBL_ELF}" ]; then
+#  MBBL_ELF="${HOME}/dmitri_io_1_GNET/mblaze/SDK/SDK_Workspace/mbbl/Release/mbbl.elf"
+#fi
 
 echo "Maintenance device tree (${DMITRI_IO_MAINT_DTS})..."
 ../dtc/dtc -f -o dt-maint.dtb -O dtb "${DMITRI_IO_MAINT_DTS}" 2>/dev/null
 echo "AVB device tree (${DMITRI_IO_AVB_DTS})..."
 ../dtc/dtc -f -o dt-avb.dtb -O dtb "${DMITRI_IO_AVB_DTS}" 2>/dev/null
-echo "AVB device tree (${DMITRI_DGPIO_AVB_DTS})..."
-../dtc/dtc -f -o dt-avb-dgpio.dtb -O dtb "${DMITRI_DGPIO_AVB_DTS}" 2>/dev/null
+#echo "AVB device tree (${DMITRI_DGPIO_AVB_DTS})..."
+#../dtc/dtc -f -o dt-avb-dgpio.dtb -O dtb "${DMITRI_DGPIO_AVB_DTS}" 2>/dev/null
 
 echo "Kernel..."
 gzip -9c ../uClinux-dist/linux-2.6.x/arch/microblaze/boot/linux.bin  > linux.bin.gz
@@ -52,8 +44,6 @@ echo "rootfs image..."
     cd ../uClinux-dist/romfs
     find . |cpio -o -H newc |gzip -9 > ../../local/romfs.bin.gz
 )
-
-
 
 ../../mbbl/mbbl-mkbootimage/pad-file -b 256 -s 8 linux.bin.gz
 ../../mbbl/mbbl-mkbootimage/pad-file -b 256 -s 8 logo-1.bin.gz
@@ -113,26 +103,26 @@ else
     echo "***********************************************************************"
 fi
 
-if [ -f "${DMITRI_DGPIO_AVB_DOWNLOAD_BIT}" ]
-    then
-    ../../mbbl/mbbl-mkbootimage/pad-file -x -b 256 -s 32 "${DMITRI_DGPIO_AVB_DOWNLOAD_BIT}"
-    echo "AVB tar file... " ${DMITRI_DGPIO_AVB_DOWNLOAD_BIT}
-    if [ -d update ]
-	then
-	rm -rf update
-    fi
-    mkdir -p update
-    cp "${DMITRI_DGPIO_AVB_DOWNLOAD_BIT}" update/download.bit
-    cp dt-avb-dgpio.dtb update/dt.dtb
-    cp linux.bin.gz logo-1.bin.gz 8x12-font.bin.gz 16x24-font.bin.gz romfs.bin.gz  identity.txt update
-    tar czf firmware-dgpio-avb.tar.gz update
-    echo "done: Output file at $PWD/firmware-dgpio-avb.tar.gz"
-else
-    echo "***********************************************************************"
-    echo " FPGA bitstream file"
-    echo " ${DMITRI_DGPIO_AVB_DOWNLOAD_BIT} is not found."
-    echo " To build firmware-dgpio-avb.tar.gz, please generate this file from XPS"
-    echo " and re-run $0"
-    echo "***********************************************************************"
-fi
+#if [ -f "${DMITRI_DGPIO_AVB_DOWNLOAD_BIT}" ]
+#    then
+#    ../../mbbl/mbbl-mkbootimage/pad-file -x -b 256 -s 32 "${DMITRI_DGPIO_AVB_DOWNLOAD_BIT}"
+#    echo "AVB tar file... " ${DMITRI_DGPIO_AVB_DOWNLOAD_BIT}
+#    if [ -d update ]
+#	then
+#	rm -rf update
+#    fi
+#    mkdir -p update
+#    cp "${DMITRI_DGPIO_AVB_DOWNLOAD_BIT}" update/download.bit
+#    cp dt-avb-dgpio.dtb update/dt.dtb
+#    cp linux.bin.gz logo-1.bin.gz 8x12-font.bin.gz 16x24-font.bin.gz romfs.bin.gz  identity.txt update
+#    tar czf firmware-dgpio-avb.tar.gz update
+#    echo "done: Output file at $PWD/firmware-dgpio-avb.tar.gz"
+#else
+#    echo "***********************************************************************"
+#    echo " FPGA bitstream file"
+#    echo " ${DMITRI_DGPIO_AVB_DOWNLOAD_BIT} is not found."
+#    echo " To build firmware-dgpio-avb.tar.gz, please generate this file from XPS"
+#    echo " and re-run $0"
+#    echo "***********************************************************************"
+#fi
 
